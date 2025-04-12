5e4a3f04b95453c607e3fb95bd23faa14982c849
Chinzzii <chinmay.singhania@gmail.com>
2025-04-12 00:38:58 -0400

    api/v1 added

--- api/v1/deepcopy.go
+++ api/v1/deepcopy.go
@@ -1,1 +1,38 @@
 package v1
+
+import "k8s.io/apimachinery/pkg/runtime"
+
+// DeepCopyInto copies all properties from the receiver to the given object of the same type.
+func (in *StaticPage) DeepCopyInto(out *StaticPage) {
+	out.TypeMeta = in.TypeMeta
+	out.ObjectMeta = in.ObjectMeta
+	out.Spec = StaticPageSpec{
+		Contents: in.Spec.Contents,
+		Image:    in.Spec.Image,
+		Replicas: in.Spec.Replicas,
+	}
+}
+
+// DeepCopyObject returns a generically typed copy of the receiver.
+func (in *StaticPage) DeepCopyObject() runtime.Object {
+	out := StaticPage{}
+	in.DeepCopyInto(&out)
+
+	return &out
+}
+
+// DeepCopyObject returns a generically typed copy of the receiver.
+func (in *StaticPageList) DeepCopyInto() runtime.Object {
+	out := StaticPageList{}
+	out.TypeMeta = in.TypeMeta
+	out.ListMeta = in.ListMeta
+
+	if in.Items != nil {
+		out.Items = make([]StaticPage, len(in.Items))
+		for i := range in.Items {
+			in.Items[i].DeepCopyInto(&out.Items[i])
+		}
+	}
+
+	return &out
+}
--- api/v1/staticpage.go
+++ api/v1/staticpage.go
@@ -1,1 +1,38 @@
 package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type StaticPageList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []StaticPage `json:"items"`
+}
+
+// DeepCopyObject implements runtime.Object.
+func (in *StaticPageList) DeepCopyObject() runtime.Object {
+	panic("unimplemented")
+}
+
+// GetObjectKind implements runtime.Object.
+// Subtle: this method shadows the method (TypeMeta).GetObjectKind of StaticPageList.TypeMeta.
+func (in *StaticPageList) GetObjectKind() schema.ObjectKind {
+	panic("unimplemented")
+}
+
+type StaticPage struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec StaticPageSpec `json:"spec"`
+}
+
+type StaticPageSpec struct {
+	Contents string `json:"contents"`
+	Image    string `json:"image"`
+	Replicas int    `json:"replicas"`
+}
